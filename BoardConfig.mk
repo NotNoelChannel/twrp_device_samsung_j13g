531f8891dff483a4bebdfd4d58d309aa1793dc03
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-05-20 14:18:07 +0200

    we try to compile the kernel from sources

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,19 +10,19 @@ TARGET_CPU_SMP                  := true
 ARCH_ARM_HAVE_TLS_REGISTER      := true
 
 TARGET_BOOTLOADER_BOARD_NAME    := SC7727S
-TARGET_PREBUILT_KERNEL          := device/samsung/j13g/prebuilt/kernel
-
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j13g/prebuilt/dt.img
+TARGET_KERNEL_CONFIG            := teamwin_j13g_defconfig
+TARGET_KERNEL_SOURCE            := kernel/samsung/j13g
+BOARD_KERNEL_CMDLINE            := console=ttyS1,115200n8
+BOARD_KERNEL_BASE               := 0x00000000
+BOARD_KERNEL_PAGESIZE           := 2048
+BOARD_MKBOOTIMG_ARGS            := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j13g/prebuilt/dt.img
 
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1085440000
 BOARD_FLASH_BLOCK_SIZE           := 1024
- TARGET_USERIMAGES_USE_EXT4      := true
+TARGET_USERIMAGES_USE_EXT4       := true
 
 
 TW_THEME                           := portrait_hdpi
